89e92ff3901903755ff6c75b8284660fc17ecbf6
Sean Mullen <srmullen13@gmail.com>
2017-03-07 19:00:24 -0700

    parse query params

--- src/index.js
+++ src/index.js
@@ -10,22 +10,17 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 window.Tone = Tone;
+window.paper = paper;
 
 const PI = Math.PI;
 const {Rectangle, Circle, Line} = paper.Path;
 const {Point} = paper;
 
 paper.setup(document.getElementById("tile-canvas"));
-window.paper = paper;
 
-const params = {
-    radius: 100,
-    theta: 90,
-    length: 100,
-    inferLength: false,
-    delta: 0.1,
-    polygonSides: 4
-};
+const INFER_LENGTH = "inferLength";
+
+const params = getParams();
 
 const grid = {
     x: 100,
@@ -39,30 +34,63 @@ const self = {
     render
 };
 
-const gui = new dat.GUI();
-const sizeController = gui.add(params, "radius", 1, 400);
-const hankinAngleController = gui.add(params, "theta", 90, 270);
-const hankinDistanceController = gui.add(params, "delta", 0, 1);
-const hankinLengthController = gui.add(params, "length", 0, 500);
-const inferLengthController = gui.add(params, "inferLength");
-const polygonSidesController = gui.add(params, "polygonSides", 3, 30);
-sizeController.onChange(render);
-hankinAngleController.onChange(render);
-hankinDistanceController.onChange(render);
-hankinLengthController.onChange(render);
-inferLengthController.onChange(render);
-polygonSidesController.onChange(render);
+function getParams () {
+    const defaults = {
+        radius: 100,
+        theta: 90,
+        length: 100,
+        [INFER_LENGTH]: false,
+        delta: 0.1,
+        polygonSides: 4
+    };
 
-const gridFolder = gui.addFolder("Grid");
-const gridXController = gridFolder.add(grid, "x", 5, 500);
-const gridYController = gridFolder.add(grid, "y", 5, 500);
-const gridColorController = gridFolder.addColor(grid, "color");
-const gridOpacityController = gridFolder.add(grid, "opacity", 0, 1);
-gridXController.onChange(render);
-gridYController.onChange(render);
-gridColorController.onChange(render);
-gridOpacityController.onChange(render);
+    if (window.location.search) {
+        const queryParams = window.location.search.slice(1).split("&").reduce((acc, s) => {
+            return Object.assign({}, acc, parseParam(s));
+        }, {});
 
+        return Object.assign({}, defaults, queryParams);
+    } else {
+        return defaults;
+    }
+}
+
+function parseParam (s) {
+    const [key, val] = s.split("=");
+    let v;
+    if (key === INFER_LENGTH) {
+        v = val === "true";
+    } else {
+        v = parseFloat(val);
+    }
+    return {[key]: v};
+}
+
+function createGUIControls () {
+    const gui = new dat.GUI();
+    const sizeController = gui.add(params, "radius", 1, 400);
+    const hankinAngleController = gui.add(params, "theta", 90, 270);
+    const hankinDistanceController = gui.add(params, "delta", 0, 1);
+    const hankinLengthController = gui.add(params, "length", 0, 500);
+    const inferLengthController = gui.add(params, INFER_LENGTH);
+    const polygonSidesController = gui.add(params, "polygonSides", 3, 30);
+    sizeController.onChange(render);
+    hankinAngleController.onChange(render);
+    hankinDistanceController.onChange(render);
+    hankinLengthController.onChange(render);
+    inferLengthController.onChange(render);
+    polygonSidesController.onChange(render);
+
+    const gridFolder = gui.addFolder("Grid");
+    const gridXController = gridFolder.add(grid, "x", 5, 500);
+    const gridYController = gridFolder.add(grid, "y", 5, 500);
+    const gridColorController = gridFolder.addColor(grid, "color");
+    const gridOpacityController = gridFolder.add(grid, "opacity", 0, 1);
+    gridXController.onChange(render);
+    gridYController.onChange(render);
+    gridColorController.onChange(render);
+    gridOpacityController.onChange(render);
+}
 
 function render () {
     paper.project.clear();
@@ -71,7 +99,6 @@ function render () {
     let x = 0;
     let y = 0;
     const length = !params.inferLength ? params.length : undefined;
-    console.log(length);
     for (let i = 0; x < paper.view.bounds.width; i++) {
         polygons[i] = [];
         x = i * grid.x;
@@ -104,4 +131,5 @@ function render () {
 // }
 
 render();
+createGUIControls();
 // Grid.draw()
